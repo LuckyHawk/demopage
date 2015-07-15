34d78891a57f62197d85c6f8751a0425d2aad9ed
范庆炀 <fanqingyang@beatTen.local>
2015-07-15 18:30:52 +0800

    main js change

---
+++ js/main.js
@@ -0,0 +1,62 @@
+;(function(w){
+
+	function getElementsByClassName(classname){
+		var result = [];
+		var tmpresult;
+		if(document.getElementsByClassName)
+		{
+			result = document.getElementsByClassName(classname);
+			return result;
+		}else{
+			tmpresult = document.getElementsByTagName('*');
+			for(var i in tmpresult){
+				if(tmpresult[i].className.indexOf(classname) != -1){
+					result.push(tmpresult[i]);
+				}
+			}
+		}
+		return result;
+	}
+
+
+
+	var ele = document.getElementById('move');
+	var clientX,clientY,moving;
+	var mousedownHandle = function(event){
+		event = event || window.event;
+		clientX = event.clientX;
+		clientY = event.clientY;
+		moving = !0;
+	}
+	var mousemoveHandle = function(event){
+		if(!moving){
+			return;
+		}
+		event = event || window.event;
+		var newClientX = event.clientX,newClientY = event.clientY;
+		var left = parseInt(window.getComputedStyle(ele).left) || 0,
+			top = parseInt(window.getComputedStyle(ele).top) || 0;
+			
+		ele.style.left = left + (newClientX - clientX) + 'px';
+		ele.style.top = top + (newClientY - clientY) + 'px';
+		clientX = newClientX;
+		clientY = newClientY;
+	}
+	var mouseupHandle = function(){
+		moving = !1;
+	}
+
+	ele.addEventListener('mousedown',mousedownHandle,false);
+	ele.addEventListener('mousemove',mousemoveHandle,false);
+	ele.addEventListener('mouseup',mouseupHandle,false);
+
+	// var color = 0xffffff;
+	// function changeColor(){
+	// 	if(parseInt(color,16) < 0)
+	// 		clearInterval(intervalid);
+	// 	ele.style.backgroundColor = '#' + (color - ffffff).toString(16);
+	// 	color = '0x' + (color - 1).toString(16);
+		
+	// }
+	// var intervalid = setInterval(function(){changeColor()},300)
+})(window)
