551ee7a01890d0a092cf77cfe37979513ff6b518
范庆炀 <fanqingyang@beatTen.local>
2015-07-20 11:27:55 +0800

    调整滑动

--- js/slide.js
+++ js/slide.js
@@ -24,12 +24,12 @@ SlideComponent.prototype = {
 		var a_r = document.createElement('a');
 		a_l.className = "Larrow fl";
 		a_l.href = "javascript:;";
-		a_l.textContent = "left"
+		//a_l.textContent = "left"
 		a_r.className = "Rarrow fr";
 		a_r.href = "javascript:;";
-		a_r.textContent = "right";
+		//a_r.textContent = "right";
 
-		this.left = parseInt(slidewrap.style.left);
+		//this.left = parseInt(slidewrap.style.left);
 		this.gap = parseInt(viewport.style.width);
 
 		slidewrap.innerHTML = '<div class="slide"><img src="./homeimages/xian.jpg" alt=""></div><div class="slide"><img src="./homeimages/jielun.jpg" alt=""></div><div class="slide"><img src="./homeimages/jielun1.jpg" alt=""></div><div class="slide"><img src="./homeimages/cheng.jpg" alt=""></div><div class="slide"><img src="./homeimages/run1.jpg" alt=""></div><div class="slide"><img src="./homeimages/run2.jpg" alt=""></div><div class="slide"><img src="./homeimages/run3.jpg" alt=""></div><div class="slide"><img src="./homeimages/run4.jpg" alt=""></div><div class="slide"><img src="./homeimages/tianjin.jpg" alt=""></div><div class="slide"><img src="./homeimages/tianjin2.jpg" alt=""></div><div class="slide"><img src="./homeimages/tianjin3.jpg" alt=""></div><div class="slide"><img src="./homeimages/tianjin4.jpg" alt=""></div><div class="slide"><img src="./homeimages/tianjin5.jpg" alt=""></div><div class="slide"><img src="./homeimages/tianjin6.jpg" alt=""></div>';
@@ -43,8 +43,9 @@ SlideComponent.prototype = {
 
 		function slideleft(){//图片向左滑动
 			if(Math.abs(parseInt(slidewrap.style.left)) + that.width < parseInt(slidewrap.style.width)){
-				that.left -= that.gap;
-				slidewrap.style.left = that.left + "px";
+				// that.left -= that.gap;
+				// slidewrap.style.left = that.left + "px";
+				that.SlideLeftSlow();
 			}else{
 				slidewrap.style.left = 0 + 'px';
 				that.left = 0;
@@ -52,8 +53,9 @@ SlideComponent.prototype = {
 		}
 		function slideright(){
 			if(parseInt(slidewrap.style.left) < 0){
-				that.left += that.gap;
-				slidewrap.style.left = that.left + "px";
+				// that.left += that.gap;
+				// slidewrap.style.left = that.left + "px";
+				that.SlideRightSlow();
 			}else{
 				slidewrap.style.left = -parseInt(slidewrap.style.width) + that.width + 'px';
 				that.left = -parseInt(slidewrap.style.width) + that.width;
@@ -62,8 +64,8 @@ SlideComponent.prototype = {
 		}
 		// this.addE(a_l);
 		// this.addE(a_r);
-		this.addEvent(a_l,'click',slideleft);
-		this.addEvent(a_r,'click',slideright);
+		this.addEvent(a_l,'click',slideright);
+		this.addEvent(a_r,'click',slideleft);
 	},
 	set_slidewrap_width:function(slidewrap){
 		var len = slidewrap.getElementsByTagName('div').length;
@@ -84,13 +86,38 @@ SlideComponent.prototype = {
 			ele.attachEvent('on'+type,handle);
 		}
 	},
-	// slideleft:function(){
-	// 	that.left -= that.gap;
-	// 	slidewrap.style.left = that.left + "px";
-	// },
-	// slideright:function(){
-	// 	that.left += that.gap;
-	// 	slidewrap.style.left = that.left + "px";
-	// }
+
+	SlideLeftSlow:function(){
+		var cur_left = parseInt(slidewrap.style.left);
+		var cur_gap = this.gap;
+		var count = 0;
+		var sub_gap = cur_gap/20;
+
+		var setIntervalID = setInterval(function(){
+			if(count >= 20)
+				clearInterval(setIntervalID);
+			else{
+				cur_left -= sub_gap;
+				slidewrap.style.left = cur_left + "px";
+				count ++;
+			}
+		},25)
+	},
+	SlideRightSlow:function(){
+		var cur_left = parseInt(slidewrap.style.left);
+		var cur_gap = this.gap;
+		var count = 0;
+		var sub_gap = this.gap/20;
+
+		var setIntervalID = setInterval(function(){
+			if(count >= 20)
+				clearInterval(setIntervalID);
+			else{
+				cur_left += sub_gap;
+				slidewrap.style.left = cur_left + "px";
+				count ++;
+			}
+		},25)
+	}
 }
 
